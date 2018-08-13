b5bc15ed18c65ceafd39bc8468943b828dd38472
Marshall Spainhour <mkspainhour@users.noreply.github.com>
2018-08-12 19:46:48 -0500

    Final MVP

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const ui = {
   $form_contact: $("#form-contact"),
+  $button_nextTestimonial: $("#testimonials-chevron"),
+  $card_testimonials: $("#testimonials-card"),
+  $wrapper_testimonialsCardContent: $("#testimonials-card-content"),
+  $text_testimonial: $("#testimonial-text"),
+  $text_testimonialAttribution: $("#testimonial-attribution")
 }
 
 
@@ -11,7 +16,17 @@ function entryPoint() {
 }
 
 function configureEventListeners() {
-  ui.$form_contact.on("submit", function(event) {
+  ui.$form_contact.on("submit", function() {
     console.log("Form submit triggered.");
   });
+  ui.$button_nextTestimonial.on("click", function(event) {
+    //showNextTestimonial();
+  });
+}
+
+let currentTestimonial = 1;
+function showNextTestimonial() {
+  ui.$card_testimonials.css("min-width", ui.$card_testimonials.outerWidth())
+  ui.$card_testimonials.css("min-height", ui.$card_testimonials.outerHeight());
+  ui.$card_testimonials.children().fadeOut(500);
 }
