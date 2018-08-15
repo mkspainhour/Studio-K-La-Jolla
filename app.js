fee5b100c4455ec7a6e25822c57d73bfba86139a
Marshall Spainhour <mkspainhour@users.noreply.github.com>
2018-08-14 21:43:05 -0500

    hid navigation, auto-scrolling contact button, media query refactoring

--- app.js
+++ app.js
@@ -17,20 +17,27 @@ let testimonialCollection = [
 const ui = {
   currentTestimonial: 0,
 
+  $button_masthead: $("#masthead-content-button"),
+
+  $pane_contact: $("#pane-contact"),
+
   $button_nextTestimonial: $("#testimonials-chevron"),
-  $wrapper_testimonial: $("#testimonials-card-content-wrapper"),
+  $card_testimonials: $("#testimonials-card"),
   $text_testimonial: $("#js-testimonial-text"),
   $text_attribution: $("#js-testimonial-attribution")
 }
 
 
 
-$(configureEventListeners);
+$(entryPoint);
+
+function entryPoint() {
+  configureEventListeners();
+}
 
 function configureEventListeners() {
-  ui.$button_nextTestimonial.on("click", function(event) {
-    showNextTestimonial();
-  });
+  ui.$button_nextTestimonial.on("click", showNextTestimonial);
+  ui.$button_masthead.on("click", jumpToContactSection);
 }
 
 function showNextTestimonial() {
@@ -39,10 +46,19 @@ function showNextTestimonial() {
   }
   let newTestimonial = testimonialCollection[ui.currentTestimonial+1];
 
-  ui.$wrapper_testimonial.fadeOut(750, function() {
+  ui.$card_testimonials.fadeOut(700, function() {
     ui.$text_testimonial.html(newTestimonial.testimonial);
     ui.$text_attribution.html(newTestimonial.attribution);
     ui.currentTestimonial++;
-    ui.$wrapper_testimonial.fadeIn(750);
+    ui.$card_testimonials.fadeIn(700);
   });
+}
+
+function jumpToContactSection() {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: ui.$pane_contact.offset().top * 0.95
+    },
+    1500
+  );
 }
