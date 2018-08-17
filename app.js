463548fbd7aee93dcfd894561ce68dfc73c8fa1d
Marshall Spainhour <mkspainhour@users.noreply.github.com>
2018-08-17 15:11:28 -0500

    commenting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+// Can be expanded with any number of additional testimonials
 let testimonialCollection = [
   {
     testimonial: `"As someone who has spent his life time obsessed with improving, healing, and strengthening my body and mind, Kurt is without a doubt the "trainer" I have had the most fulfilling and longest working relationship. His profound and varied knowledge base is almost secondary to his intuitive skills that begin with how to make someone feel comfortable enough to move and extends to knowing how to deeply impact bodily change. He managed to do all this in an easy going and playful manner that caused me to cherish those times in the week when we could work together. New York's loss is San Diego's gain."`,
@@ -20,24 +21,32 @@ const ui = {
   $button_masthead: $("#masthead-content-button"),
 
   $pane_contact: $("#pane-contact"),
-
-  $button_nextTestimonial: $("#testimonials-chevron"),
+  
   $card_testimonials: $("#testimonials-card"),
   $text_testimonial: $("#js-testimonial-text"),
-  $text_attribution: $("#js-testimonial-attribution")
+  $text_attribution: $("#js-testimonial-attribution"),
+  $button_nextTestimonial: $("#testimonials-chevron")
 }
 
 
 
 $(entryPoint);
-
 function entryPoint() {
   configureEventListeners();
 }
 
+
 function configureEventListeners() {
   ui.$button_masthead.on("click", jumpToContactSection);
   ui.$button_nextTestimonial.on("click", showNextTestimonial);
+}
+
+function jumpToContactSection() {
+  $( [document.documentElement, document.body] ).animate(
+    {
+      scrollTop: ui.$pane_contact.offset().top * 0.95 //The y-axis position of the top of the element, less 5%
+    },
+    1500);
 }
 
 function showNextTestimonial() {
@@ -53,13 +62,4 @@ function showNextTestimonial() {
     ui.currentTestimonial++;
     ui.$card_testimonials.fadeIn(700);
   });
-}
-
-function jumpToContactSection() {
-  $([document.documentElement, document.body]).animate(
-    {
-      scrollTop: ui.$pane_contact.offset().top * 0.95 //The y-axis position of the top of the element, less 5%
-    },
-    1500
-  );
 }
